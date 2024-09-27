7971af2b55a15295943ea75a69eae23f988a10cc
vincdeprinz <vincentstucki9@gmail.com>
2024-09-27 13:56:11 +0200

    styling

--- src/App.js
+++ src/App.js
@@ -6,20 +6,38 @@ import AltstadtNight from './galerie/altstadt_Night.jpg'
 import AltstadtAlt from './galerie/altstadt_alt.jpg'
 import Carousel from './carousel.js';
 import Orte from './orte.js';
+import { motion, useAnimation } from 'framer-motion';
 
 function App() {
   return (
     <div>
       <nav className='navbar'>
-          damn
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#geschichte">Geschichte</a></li>
+          <li><a href="#orte">Orte</a></li>
+          <li><a href="#video">Video</a></li>
+          <li><a href="#galerie">Galerie</a></li>
+          <li><a href="#karte">Karte</a></li>
+        </ul>
       </nav>
-      <header className='intro'>
-        <h1>Altstadt Winterthur</h1>
-        
+      <header className='intro' id="home">
+      <motion.h1
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+        Altstadt Winterthur
+        </motion.h1>
       </header>
 
-      <section className='section dark history'>
-        <div className="history-content">
+      <section className='section dark history' id="geschichte">
+        <motion.div className="history-content"
+        initial={{ opacity: 0, x: -100 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        
           <div className="text">
             <h2>Geschichte</h2>
             <p>Ein Überblick über die Altstadt.</p>
@@ -27,10 +45,15 @@ function App() {
           <div className="image">
           <img src={AltstadtAir} alt="Geschichte Bild" />
           </div>
-        </div>
+        </motion.div>
       </section>
 
-      <section className='section'>
+      <section className='section' id="orte">
+      <motion.div
+          initial={{ opacity: 0, y: 100 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
         <h2>Orte</h2>
         <div className="grid-container">
           <div className="grid-item image">
@@ -50,12 +73,17 @@ function App() {
               <img src={AltstadtAir} alt="in der Luft" />
             </div>
           </div>
+          </motion.div>
       </section>
       
-      <section className='section dark'>
+      <section className='section dark' id="video">
+      <motion.div
+          initial={{ opacity: 0, x: 100 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5 }}
+        >
         <div className="content-block">
           <h2>Erkunden Sie die Altstadt</h2>
-          <h3>Video über die Altstadt</h3>
           <div className="video">
               <video controls>
                 <source src="./videos/altstadt_video.mp4" type="video/mp4" />
@@ -63,11 +91,22 @@ function App() {
               </video>
           </div>
         </div>
+        </motion.div>
       </section>
-
+      <motion.div id="galerie"
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
       <Carousel />
+      </motion.div>
       <section className='section dark'>
-        <div className="map">
+      <motion.div
+        initial={{ opacity: 0, x: -100 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        <div className="map" id="karte">
           <h3>Altstadt auf der Karte</h3>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.3373041095627!2d9.037387315675263!3d47.49881247917762!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479aa4a2e5a3be3f%3A0x420b0c58ddad08dd!2sAltstadt%2C%208400%20Winterthur!5e0!3m2!1sen!2sch!4v1601495805947!5m2!1sen!2sch"
@@ -78,6 +117,7 @@ function App() {
             title="Altstadt Karte"
           ></iframe>
         </div>
+        </motion.div>
       </section>
 
       <section className='section'>
